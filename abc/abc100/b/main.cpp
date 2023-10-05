1ff78bc51b8d75689b47ea496fdfd4200d629b11
endo1322 <bondroid3.22@icloud.com>
2023-10-05 17:14:36 +0900

    ac

---
+++ abc/abc100/b/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define rrep(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define rrep3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+vector<int> culc_p(int N) {
+    vector<bool> isP(N, true);
+    vector<int> P;
+    isP[0] = isP[1] = false;
+    rep3 (i, 2, N) {
+        if (!isP[i]) continue;
+        P.push_back(i);
+        for (int j = 1; i * j < N; j++) {
+            isP[i*j] = false;
+        }
+    }
+    return P;
+}
+
+signed main() {
+    // input
+    int D, N; cin >> D >> N;
+
+    // solve
+    int ans;
+    if (N == 100) {
+        ans = pow(100, D+1) + pow(100, D);
+    } else {
+        ans = pow(100, D) * N;
+    }
+
+    // output
+    cout << ans << endl;
+}
