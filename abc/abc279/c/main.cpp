445c6d7f8c216a56b38222e317b9409b094690c8
bondroid1322 <bondroid3.22@icloud.com>
2024-09-08 16:31:53 +0900

    ac

---
+++ abc/abc279/c/main.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define rrep(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define rrep3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template<class T> using V = vector<T>;
+template<class T> using P = pair<T, T>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T,V<T>,greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+set<int> culc_p(int N) {
+    vector<bool> isP(N+1, true);
+    set<int> P;
+    isP[0] = isP[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (!isP[i]) continue;
+        for (int j = i * i; j <= N; j += i) isP[j] = false;
+    }
+    rep3 (i, 2, N+1) if (isP[i]) P.insert(i);
+    return P;
+}
+
+pair<int, int> add_pair(const pair<int, int>& p1, const pair<int, int>& p2) {
+    pair<int, int> ret;
+    ret.first = p1.first + p2.first;
+    ret.second = p1.second + p2.second;
+    return ret;
+}
+
+int binaryToDecimal(const string& S) {
+    int res = 0;
+    for (char c: S) res = res * 2 + (c - '0');
+    return res;
+}
+
+signed main() {
+    // input
+    int H, W; cin >> H >> W;
+    vector<vector<char>> S(H, vector<char>(W));
+    vector<vector<char>> T(H, vector<char>(W));
+    rep (i, H) rep (j, W) cin >> S[i][j];
+    rep (i, H) rep (j, W) cin >> T[i][j];
+
+    // solve
+    string ans = "No";
+    multiset<string> Sset, Tset;
+    rep (i, W) {
+        string s_tmp = "";
+        string t_tmp = "";
+        rep (j, H) {
+            s_tmp += S[j][i];
+            t_tmp += T[j][i];
+        }
+        Sset.insert(s_tmp);
+        Tset.insert(t_tmp);
+    }
+
+    if (Sset == Tset) ans = "Yes";
+
+    // output
+    cout << ans << endl;
+}
---
+++ abc/abc279/d/main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define rrep(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define rrep3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template<class T> using V = vector<T>;
+template<class T> using P = pair<T, T>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T,V<T>,greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+set<int> culc_p(int N) {
+    vector<bool> isP(N+1, true);
+    set<int> P;
+    isP[0] = isP[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (!isP[i]) continue;
+        for (int j = i * i; j <= N; j += i) isP[j] = false;
+    }
+    rep3 (i, 2, N+1) if (isP[i]) P.insert(i);
+    return P;
+}
+
+pair<int, int> add_pair(const pair<int, int>& p1, const pair<int, int>& p2) {
+    pair<int, int> ret;
+    ret.first = p1.first + p2.first;
+    ret.second = p1.second + p2.second;
+    return ret;
+}
+
+int binaryToDecimal(const string& S) {
+    int res = 0;
+    for (char c: S) res = res * 2 + (c - '0');
+    return res;
+}
+
+signed main() {
+    // input
+    double A, B; cin >> A >> B;
+
+    // solve
+    auto f = [&](int x) -> double {
+        double res = 0;
+        return (double)(A / sqrt(x + 1)) + (double)(B * x); 
+    };
+
+    int left = 0;
+    int right = 1e18;
+    double ans = 0;
+    while (right - left > 3) {
+        int m1 = (left * 2 + right) / 3;
+        int m2 = (left + right * 2) / 3;
+
+        if (f(m1) > f(m2)) left = m1;
+        else right = m2;
+    }
+    
+    ans = INF;
+    rep3 (i, left-10, right+10) {
+        ans = min(ans, f(i));
+    }
+
+    // output
+    cout << fixed << setprecision(10) << ans << endl;
+}
