a31d2d29f5e210598be8939203796f7572c438b2
bondroid1322 <bondroid3.22@icloud.com>
2024-10-20 18:25:26 +0900

    ac

---
+++ abc/abc376/d/main.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define rrep(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define rrep3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template<class T> using V = vector<T>;
+template<class T> using P = pair<T, T>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T,V<T>,greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+set<int> culc_p(int N) {
+    vector<bool> isP(N+1, true);
+    set<int> P;
+    isP[0] = isP[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (!isP[i]) continue;
+        for (int j = i * i; j <= N; j += i) isP[j] = false;
+    }
+    rep3 (i, 2, N+1) if (isP[i]) P.insert(i);
+    return P;
+}
+
+int binary_search(int key, vector<int> &a) {
+    int ng = -1;
+    int ok = (int)a.size();
+
+    auto isOK = [&](int index) -> bool {
+        return a[index] >= key;
+    };
+
+    while (abs(ok - ng) > 1) {
+        int mid = (ok + ng) / 2;
+
+        if (isOK(mid)) ok = mid;
+        else ng = mid;
+    }
+    return ok;
+}
+
+pair<int, int> add_pair(const pair<int, int>& p1, const pair<int, int>& p2) {
+    pair<int, int> ret;
+    ret.first = p1.first + p2.first;
+    ret.second = p1.second + p2.second;
+    return ret;
+}
+
+int binaryToDecimal(const string& S) {
+    int res = 0;
+    for (char c: S) res = res * 2 + (c - '0');
+    return res;
+}
+
+signed main() {
+    // input
+    int N, M; cin >> N >> M;
+    vector<vector<int>> Graph(N);
+    rep (i, M) {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        Graph[a].push_back(b);
+    }
+
+    // solve
+    int ans = INF;
+    vector<int> dist(N, INF);
+    queue<int> que;
+    dist[0] = 0;
+    que.push(0);
+    while (!que.empty()) {
+        int v = que.front(); que.pop();
+        for (int u: Graph[v]) {
+            if (dist[u] != INF) continue;
+            dist[u] = dist[v] + 1;
+            que.push(u);
+        }
+    }
+    rep (i, N) {
+        for (auto u: Graph[i]) {
+            if (u == 0 && dist[i] != INF) ans = min(ans, dist[i] + 1);
+        }
+    }
+    if (ans == INF) ans = -1;
+
+    // output
+    cout << ans << endl;
+}
