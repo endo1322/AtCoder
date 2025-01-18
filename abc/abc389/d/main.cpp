bbdbd34d275aeea437f21e2d09f3e60d8adb7d75
bondroid1322 <bondroid3.22@icloud.com>
2025-01-18 22:43:24 +0900

    927

---
+++ abc/abc389/d/main.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define rrep(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define rrep3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template<class T> using V = vector<T>;
+template<class T> using P = pair<T, T>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T,V<T>,greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<double, double>> D4 = {{0.5, 0.5}, {0.5, -0.5}, {-0.5, 0.5}, {-0.5, -0.5}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+set<int> culc_p(int N) {
+    vector<bool> isP(N+1, true);
+    set<int> P;
+    isP[0] = isP[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (!isP[i]) continue;
+        for (int j = i * i; j <= N; j += i) isP[j] = false;
+    }
+    rep3 (i, 2, N+1) if (isP[i]) P.insert(i);
+    return P;
+}
+
+// めぐる式二分探索
+// (ng, ok], [ok, ng) で考える
+// https://qiita.com/drken/items/97e37dd6143e33a64c8c#2-%E4%B8%80%E8%88%AC%E5%8C%96%E3%81%97%E3%81%9F%E4%BA%8C%E5%88%86%E6%8E%A2%E7%B4%A2%E6%B3%95
+int binary_search(int j, int R) {
+    int ng = R + 1;
+    int ok = 0;
+
+    auto isOK = [&](int i) -> bool {
+        bool flag = true;
+        for (auto d: D4) {
+            double x = d.first + i;
+            double y = d.second + j;
+            double dist = sqrt(x * x + y * y);
+            if (dist > R) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    };
+
+    while (abs(ok - ng) > 1) {
+        int mid = (ok + ng) / 2;
+
+        if (isOK(mid)) ok = mid;
+        else ng = mid;
+    }
+    return ok;
+}
+
+pair<int, int> add_pair(const pair<int, int>& p1, const pair<int, int>& p2) {
+    pair<int, int> ret;
+    ret.first = p1.first + p2.first;
+    ret.second = p1.second + p2.second;
+    return ret;
+}
+
+int binaryToDecimal(const string& S) {
+    int res = 0;
+    for (char c: S) res = res * 2 + (c - '0');
+    return res;
+}
+
+signed main() {
+    // input
+    int R; cin >> R;
+
+    // solve
+    int ans = 0;
+    int ans1 = 0;
+    int ans2 = 0;
+    ans1 = binary_search(0, R); 
+    rep3 (j, 1, R) {
+        int tmp = binary_search(j, R);
+        ans2 += tmp;
+    }
+    ans = ans1 * 4 + ans2 * 4 + 1;
+
+    // output
+    cout << ans << endl;
+}
