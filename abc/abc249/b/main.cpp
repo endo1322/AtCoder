38c565c68e43326afd6e827deb8542bac857a443
bondroid1322 <bondroid3.22@icloud.com>
2025-02-13 22:16:05 +0900

    ac

---
+++ abc/abc249/b/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define rrep(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define rrep3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template<class T> using V = vector<T>;
+template<class T> using P = pair<T, T>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T,V<T>,greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+// def isUpper
+
+signed main() {
+    // input
+    string S;
+    cin >> S;
+
+    // solve
+    bool ans = false;
+    bool isUpper = false;
+    bool isLower = false;
+    set<char> tmp;
+
+    for (auto s: S) {
+        if (isupper(s)) isUpper = true;
+        if (islower(s)) isLower = true;
+        tmp.insert(s);
+    }
+
+    if (isUpper && isLower && tmp.size() == S.size()) ans = true;
+
+    // output
+    if (ans) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
