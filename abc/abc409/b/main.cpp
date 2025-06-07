9fa589f0d8382e1c440845959781233fd64cd6d7
bondroid1322 <bondroid3.22@icloud.com>
2025-06-07 22:38:22 +0900

    890

---
+++ abc/abc409/b/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define rep3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define rrep(i, n) for (int i = (int)(n) - 1; (i) >= 0; --(i))
+#define rrep3(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); --(i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template <class T>
+using V = vector<T>;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using PQ = priority_queue<T>;
+template <class T>
+using PQG = priority_queue<T, V<T>, greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+signed main()
+{
+    // input
+    int N;
+    cin >> N;
+    // set<int> st;
+    vector<int> A(N);
+    rep(i, N)
+    {
+        int a;
+        cin >> a;
+        A[i] = a;
+        // st.insert(a);
+    }
+
+    // solve
+    int ans = 0;
+    int prev = 0;
+    sort(all(A));
+    rep(i, N)
+    {
+        if (A[i] == prev)
+            continue;
+        prev = A[i];
+        if (N - i >= A[i])
+        {
+            ans = A[i];
+        }
+        else
+        {
+            ans = max(ans, N - i);
+            break;
+        }
+    }
+
+    // output
+    cout << ans << endl;
+}
---
+++ abc/abc409/c/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define rep3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define rrep(i, n) for (int i = (int)(n) - 1; (i) >= 0; --(i))
+#define rrep3(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); --(i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template <class T>
+using V = vector<T>;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using PQ = priority_queue<T>;
+template <class T>
+using PQG = priority_queue<T, V<T>, greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+signed main()
+{
+    // input
+    int N, L;
+    cin >> N >> L;
+    map<int, int> M;
+    int prev = 0;
+    M[0] = 1;
+    rep(i, N - 1)
+    {
+        int a;
+        cin >> a;
+        int n = (prev + a) % L;
+        M[n]++;
+        prev = n;
+    }
+
+    // solve
+    int ans = 0;
+
+    if (L % 3 != 0)
+    {
+        cout << ans << endl;
+        return 0;
+    }
+    int diff = L / 3;
+    rep(i, L)
+    {
+        ans += M[i] * M[(i + diff) % L] * M[(i + 2 * diff) % L];
+    }
+
+    // output
+    cout << ans / 3 << endl;
+}
