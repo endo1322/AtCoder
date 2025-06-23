473e3c73e97d475ba38920d9087b48f2dfa9153b
bondroid1322 <bondroid3.22@icloud.com>
2025-06-23 15:52:18 +0900

    ac

---
+++ abc/abc402/d/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define rep3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define rrep(i, n) for (int i = (int)(n) - 1; (i) >= 0; --(i))
+#define rrep3(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); --(i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template <class T>
+using V = vector<T>;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using PQ = priority_queue<T>;
+template <class T>
+using PQG = priority_queue<T, V<T>, greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+signed main()
+{
+    // input
+    int N, M;
+    cin >> N >> M;
+    V<int> ct(N, 0);
+    rep(i, M)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        ct[(a + b) % N]++;
+    }
+
+    // solve
+    int ans = M * (M - 1) / 2;
+    rep(i, N)
+    {
+        ans -= ct[i] * (ct[i] - 1) / 2;
+    }
+
+    // output
+    cout << ans << endl;
+}
