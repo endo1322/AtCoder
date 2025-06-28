66d91f3efa5d05af056500d7e0031f5d7709a5bc
bondroid1322 <bondroid3.22@icloud.com>
2025-06-28 23:51:34 +0900

    828

---
+++ abc/abc412/c/main.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define rep3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define rrep(i, n) for (int i = (int)(n) - 1; (i) >= 0; --(i))
+#define rrep3(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); --(i))
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+#define int long long
+#define ll long long
+#define double long double
+using mint = modint998244353;
+template <class T>
+using V = vector<T>;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using PQ = priority_queue<T>;
+template <class T>
+using PQG = priority_queue<T, V<T>, greater<T>>;
+const long long INF = LLONG_MAX;
+const long long MIN_INF = LLONG_MIN;
+
+vector<pair<int, int>> D4 = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> D8 = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+signed main()
+{
+    // input
+    int T;
+    cin >> T;
+    V<int> Ans(T);
+    rep(i, T)
+    {
+        int N;
+        cin >> N;
+        V<int> S(N - 2);
+        int cur, gl;
+        rep(j, N)
+        {
+            int s;
+            cin >> s;
+            if (j == 0)
+            {
+                cur = s;
+                continue;
+            }
+            if (j == N - 1)
+            {
+                gl = s;
+                continue;
+            }
+            S[j - 1] = s;
+        }
+        sort(all(S));
+        int ct = 0;
+        bool ok = false;
+        while (true)
+        {
+            if (cur * 2 >= gl)
+            {
+                ok = true;
+                break;
+            }
+
+            auto it = upper_bound(all(S), cur * 2);
+            if (it == S.begin())
+                break;
+
+            --it;
+            cur = *it;
+            S.erase(it);
+            ct++;
+        }
+        Ans[i] = ok ? ct + 2 : -1;
+    }
+
+    // output
+    rep(i, T)
+    {
+        cout << Ans[i] << endl;
+    }
+}
